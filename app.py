3f5cc94f797b41ac1bc70c0693aa83f4a9ef40ad
Urszula Maciaszek <maciaszek89@gmail.com>
2019-03-03 21:22:34 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
+from sqlalchemy.sql import func
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI']="postgresql://postgres:123@localhost/collector"
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://bnlrbctqwwwvhd:35532c4e5a9cdf7e96a2ab86c4dce2a958a7b34b681f4be6e5342a55e122a333@ec2-54-83-55-115.compute-1.amazonaws.com:5432/dbo0rlejhl8ie3?sslmode=require'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db=SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__="data"
+    id=db.Column(db.Integer, primary_key=True)
+    email_=db.Column(db.String(120), unique=True)
+    height_=db.Column(db.Integer)
+    def __init__(self, email_, height_):
+        self.email_=email_
+        self.height_=height_
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success", methods=["POST"])
+def success():
+    if request.method =="POST":
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+        if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            average_height=db.session.query(func.avg(Data.height_)).scalar()
+            average_height=round(average_height, 1)
+            count = db.session.query(Data.height_).count()
+            send_email(email, height, average_height, count)
+            return render_template("success.html")
+    return render_template("index.html", text = "Seems like we have something from that email address already!")
+
+if __name__ =="__main__":
+    app.debug=True
+    app.run()
